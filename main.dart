c05330656eb9454e3544850c5379823fe35a2612
PutriNovitaRahmadani <137311247+PutriNovitaRahmadani@users.noreply.github.com>
2023-10-13 21:05:10 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:testing/Personal_form.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Form Input",
+      home: PersonalForm(),
+    );
+  }
+}
